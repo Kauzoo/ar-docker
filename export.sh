240bf2aca14a1d54e3d8d685f495b0a01d602f03
nyr <>
2025-05-07 15:43:53 +0200

    Fixed scritps

--- export.sh
+++ export.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-EXPORT_DIR=./ar-docker-export/
-docker create --pull --name ar-docker-export kauzoo/ar-docker:latest
+EXPORT_DIR=./ar-docker-export
+mkdir -p $EXPORT_DIR/{godot-cpp,opencv/{lib,include}}
+docker create --pull missing --name ar-docker-export kauzoo/ar-docker:latest
 echo Exporting to $EXPORT_DIR
 docker cp ar-docker-export:/home/ardocker/export/opencv/lib/ $EXPORT_DIR/opencv/lib
 docker cp ar-docker-export:/home/ardocker/export/opencv/include/ $EXPORT_DIR/opencv/include
